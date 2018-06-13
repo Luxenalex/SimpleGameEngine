f15bd56dc7b95e84eb4ecf3ed736d862b3c5ec43
ZiramD <ziramd@gmail.com>
2018-06-13 09:48:50 +0200

    Add AbstractGame class.

---
+++ src/AbstractGame.java
@@ -0,0 +1,7 @@
+public abstract class AbstractGame {
+
+    public abstract void update(SimpleGameEngine gameContainer, float deltaTime);
+
+    public abstract void render(SimpleGameEngine gameContainer, Renderer renderer)
+
+}
