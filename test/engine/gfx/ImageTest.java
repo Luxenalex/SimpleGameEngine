9d38ca6d42ed3ca50976f9ee32814c795de75733
ZiramD <ziramd@gmail.com>
2018-06-17 13:57:09 +0200

    Add tests for getting a specific pixels color Change ImageTest to take smaller image

--- test/engine/gfx/ImageTest.java
+++ test/engine/gfx/ImageTest.java
@@ -13,10 +13,47 @@ public class ImageTest {
     public void setup() {
 
         try {
-            image = new Image("/testRectangle.png");
+            image = new Image("/testColors.png");
         } catch (IOException e) {
             System.err.println("Failed to create image at setup");
         }
+    }
+
+    private int[] getOriginalColors() {
+        return new int[] {
+                0xFFFFFFFF,
+                0xFFFFFFFF, //First FF is for alpha.
+                0xFF0000FF,
+                0xFF0000FF,
+                0xFFFFFFFF,
+                0xFFFFFFFF,
+                0xFF00FFFF,
+                0xFF00FFFF,
+                0xFFFFFFFF,
+                0xFFFFFFFF,
+                0xFFFF00FF,
+                0xFFFF00FF,
+                0xFFFFFFFF,
+                0xFFFFFFFF,
+                0xFF000000,
+                0xFF000000,
+                0xFFFFFFFF,
+                0xFFFFFFFF,
+                0xFF00FF00,
+                0xFF00FF00,
+                0xFFFFFFFF,
+                0xFFFFFFFF,
+                0xFFFFFF00,
+                0xFFFFFF00,
+                0xFFFFFFFF,
+                0xFFFFFFFF,
+                0xFFFF0000,
+                0xFFFF0000,
+                0xFFFFFFFF,
+                0xFFFFFFFF,
+                0xFF808080,
+                0xFF808080
+        };
     }
 
     @After
@@ -25,22 +62,48 @@ public class ImageTest {
     }
 
     @Test
-    public void shouldHaveWidth64() {
-        assertEquals(64,image.getWidth());
+    public void shouldHaveWidth2() {
+        assertEquals(2,image.getWidth());
     }
 
     @Test
-    public void shouldHaveHeight32() {
-        assertEquals(32, image.getHeight());
+    public void shouldHaveHeight16() {
+        assertEquals(16, image.getHeight());
     }
 
     @Test
-    public void shouldHave64x32Pixels() {
-        assertEquals(32*64, image.getPixels().length);
+    public void shouldHave2x16Pixels() {
+        assertEquals(2*16, image.getPixels().length);
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void imageShouldNotExist() throws IllegalArgumentException, IOException {
         new Image("/shouldNotExist.png");
+    }
+
+    @Test
+    public void imageShouldHaveCorrectColors() {
+        int[] colors = getOriginalColors();
+        assertArrayEquals(colors, image.getPixels());
+    }
+
+    @Test
+    public void shouldGetColorWhite() {
+        assertEquals(0xFFFFFFFF, image.getColor(0, 0));
+    }
+
+    @Test
+    public void shouldGetColorBlue() {
+        assertEquals(0xFF0000FF, image.getColor(1, 1));
+    }
+
+    @Test
+    public void shouldGetColorMagenta() {
+        assertEquals(0xFFFF00FF, image.getColor(0, 5));
+    }
+
+    @Test
+    public void shouldGetColorGrey() {
+        assertEquals(0xFF808080, image.getColor(1, 15));
     }
 }
